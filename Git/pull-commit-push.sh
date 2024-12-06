817fa918774a8e6dd0682916db83f917ebfd5d1f
Darren Neese <darren@superpowerlabs.co>
2024-12-06 13:27:57 -0500

    Update pull-commit-push.sh

--- Git/pull-commit-push.sh
+++ Git/pull-commit-push.sh
@@ -2,10 +2,10 @@
 git add .
 git commit -m "Committing often... $(date)"
 
-# Pull changes from Azure DevOps
-git pull azure main  
-git pull origin main  
-
 # Push to both remotes when making local changes
 git push origin main
 git push azure main
+
+# Pull changes from Azure DevOps
+git pull azure main  
+git pull origin main  
